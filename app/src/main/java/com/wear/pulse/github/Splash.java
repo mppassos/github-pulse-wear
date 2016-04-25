569e8e6437bc398310550f5625a305b9d7e64dd2
mvfsilva <marcusvinicius30798@gmail.com>
2016-04-24 23:26:53 -0300

    Create new class for start app Splash.java

--- app/src/main/java/com/wear/pulse/github/Splash.java
+++ app/src/main/java/com/wear/pulse/github/Splash.java
@@ -1,7 +1,33 @@
 package com.wear.pulse.github;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by MarcusSilva on 24/04/16.
  */
-public class Splash {
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+	  	super.onCreate(savedInstanceState);
+	  	setContentView(R.layout.splash_activity);
+
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+			finish();
+
+			Intent intent = new Intent();
+			intent.setClass(Splash.this, MainActivity.class);
+			startActivity(intent);
+			}
+		}, 3000);
+
+    }
+
 }
